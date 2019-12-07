b6147b59cc9ac7994c44deacbf11e46c71b3a6e9
SophieM <47410186+SophM@users.noreply.github.com>
2019-12-07 14:36:36 -0800

    start solving day-1 puzzle

---
+++ day_1_tyranny_rocket_equation/logic_day1.py
@@ -0,0 +1,13 @@
+import math
+
+
+def fuel_counter_upper(file):
+    f = open(file, "r")
+    fuel_total = 0
+    for line in f:
+        fuel_total = fuel_total + (math.floor(int(line)/3) - 2)
+    print(fuel_total)
+
+
+if __name__ == "__main__":
+   fuel_counter_upper("input_day1.txt")
