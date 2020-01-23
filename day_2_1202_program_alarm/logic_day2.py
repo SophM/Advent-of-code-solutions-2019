5663cd110ee6fef54ce1be0dcbb0ac00fbc2b538
SophieM <47410186+SophM@users.noreply.github.com>
2020-01-22 21:49:22 -0800

    making progress - convert list into integers

--- day_2_1202_program_alarm/logic_day2.py
+++ day_2_1202_program_alarm/logic_day2.py
@@ -1,8 +1,10 @@
 def intcode_program(file):
     f = open(file)
-    intcode = f.readline().split(",")
+    intcode = [int(i) for i in f.readline().split(",")]
     # other way to get the first line: intcode = f.readlines()[0].split(",")
     f.close()
+    # for i in range(0,len(intcode)-1):
+    #     if intcode[i] == 99
     print(intcode)
 
 
