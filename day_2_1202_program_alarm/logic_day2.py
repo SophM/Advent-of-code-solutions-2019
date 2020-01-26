0437f2ef3d1d565ac73c152f84dc9a0ab73c5c42
SophieM <47410186+SophM@users.noreply.github.com>
2020-01-25 16:38:10 -0800

    tiny changes

--- day_2_1202_program_alarm/logic_day2.py
+++ day_2_1202_program_alarm/logic_day2.py
@@ -1,11 +1,14 @@
 def intcode_program(file):
     f = open(file)
     intcode = [int(i) for i in f.readline().split(",")]
-    # other way to get the first line: intcode = f.readlines()[0].split(",")
+    # other way to transform the first line into an array of integers
+    # intcode = f.readlines()[0].split(",")
     f.close()
-    # for i in range(0,len(intcode)-1):
-    #     if intcode[i] == 99
-    print(intcode)
+    for i in range(0,len(intcode)-1, 4):
+        print(i, ": ", intcode[i])
+        # if intcode[i] == 99:
+        #     print(intcode[i])
+    # print(intcode)
 
 
 if __name__ == "__main__":
